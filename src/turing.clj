f5c6d1abbb24d08d35b3884ae1b259b243fda59c
stopachka <stepan.p@gmail.com>
2020-12-29 12:08:36 -0500

    transc

--- src/turing.clj
+++ src/turing.clj
@@ -16,6 +16,26 @@
   (turing-move-right [[] nil []])
   (turing-move-right [[nil] 1 [0]]))
 
+(defn turing-move-left [[left cur right]]
+  [(rest left)
+   (first left)
+   (list* cur right)])
+
+(comment
+  (turing-move-left [[] nil []])
+  (turing-move-left [[:x] 1 [0]]))
+
+(defn turing-erase [[left _ right]]
+  [left nil right])
+
+(comment
+  (turing-erase [[0] 1 [0]]))
+
+(defn tape->pretty-seq [[left cur right]]
+  (filter
+   identity
+   (concat (reverse left) (list (list cur)) right)))
+
 (defn turing-print [[left _ right] v]
   [left v right])
 
@@ -50,16 +70,23 @@
 (defn find-instruction [{:keys [machine current-state tape]}]
   (let [[_ current-tape _] tape]
     (first
-      (filter
-        (fn [[begin-state desired-tape]]
+     (filter
+      (fn [[begin-state desired-tape]]
+        (let [desired-tape-set (if (set? desired-tape) desired-tape #{desired-tape})]
           (and (= current-state begin-state)
-               (= current-tape desired-tape)))
-        machine))))
+               (contains? desired-tape-set current-tape))))
+      machine))))
 
 (defn apply-action [configuration action]
   (cond
+    (= action :move-left)
+    (update configuration :tape turing-move-left)
+
     (= action :move-right)
     (update configuration :tape turing-move-right)
+
+    (= action :erase)
+    (update configuration :tape turing-erase)
 
     (= (first action) :print)
     (update configuration :tape turing-print (second action))))
@@ -70,26 +97,45 @@
            :current-state
            end-state)))
 
-(defn tape->seq [[left cur right]]
-  (concat
-    (reverse left)
-    (list cur)
-    right))
-
 (defn run-loop [configuration]
   (loop [config configuration]
     (let [{:keys [tape] :as config'} (run-step config)]
-      (println (tape->seq tape))
+      (println (tape->pretty-seq tape))
       (Thread/sleep 1000)
       (recur config'))))
 
-
 (comment
-  (-> ex-configuration run-step run-step run-step :tape tape->seq)
+  (-> ex-configuration run-step run-step run-step :tape tape->pretty-seq)
   (future (run-loop ex-configuration)))
 
 
+;; transc
+
+;; let's print the sequence 001011011101111...
+
+(def transc-machine
+  [[:begin nil :-> [[:print :sentinel] :move-right
+                    [:print :sentinel] :move-right
+                    [:print 0] :move-right :move-right
+                    [:print 0] :move-left :move-left] :o]
+
+   [:o 1 :-> [:move-right [:print :todo] :move-left :move-left :move-left] :q]
+   [:o 0 :-> [] :q]
+
+   [:q #{0 1} :-> [:move-right :move-right] :q]
+   [:q nil    :-> [[:print 1] :move-left] :p]
+
+   [:p :todo     :-> [:erase :move-right] :q]
+   [:p :sentinel :-> [:move-right] :f]
+   [:p nil       :-> [:move-left :move-left] :p]
+
+   [:f #{0 1} :-> [:move-right :move-right] :f]
+   [:f nil    :-> [[:print 0] :move-left :move-left] :o]])
+
+(def transc-configuration {:machine transc-machine
+                           :current-state :begin
+                           :tape [[] nil []]})
 
 
-
-
+(comment
+  (future (run-loop transc-configuration)))
